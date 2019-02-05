a23d0d8ac8411f27bfa6439c246ca43b0abcf074
doug <dmhwebb@gmail.com>
2019-02-05 01:36:40 +0100

    fixed permalinks, removed metalsmith-markdown

--- build.js
+++ build.js
@@ -5,7 +5,6 @@
 // build.js: metalsmith API method (alternative is metalsmith.json https://github.com/segmentio/metalsmith/wiki/The-basics-of-Metalsmith)
 
 const Metalsmith    = require('metalsmith')
-const markdown      = require('metalsmith-markdown')
 const debug         = require('metalsmith-debug')
 const layouts       = require('metalsmith-layouts')
 const dateFormatter = require('metalsmith-date-formatter')
@@ -21,20 +20,21 @@ Metalsmith(__dirname)         // __dirname defined by node.js:
   .source('./src')            // source directory
   .destination('./public')    // destination directory
   .use(drafts())              // stops drafts (`draft: true`) from being built
-  .use(markdown())            // transpile md into html
+  .use(inplace({            
+      pattern: "articles/*"
+  }))
   .use(collections({          // create 'collections' metadata from `collections: blah` OR pattern, defined below
     articles: {
-      pattern: 'articles/*.html',
+      pattern: 'articles/*',
       sortBy: 'date',
       reverse: true
     }
   }))
-  .use(permalinks())          // prettifies urls... but adds everything into it's own folder and calls it index.html... why?
   .use(dateFormatter({        // format date/time based on YFM key and 'moment' date formats: http://momentjs.com/
     dates: [
       {
         key: 'date',          // change the standard date format, seems better than the default one
-        format: 'Do MMM YYYY'
+        format: 'Do MMM YYYY' // e.g. 27th Nov 2018
       },
       {
         key: 'modifiedDate',
@@ -43,9 +43,10 @@ Metalsmith(__dirname)         // __dirname defined by node.js:
     ]
   }))
   .use(wordcount())
+  .use(permalinks())           // prettifies urls... but adds everything into it's own folder and calls it index.html... why?
   .use(inplace())              // transform `contents` based on RTL file extenstions. Only necessary if inheriting template bits! Use absolute path.
-  .use(layouts({
-    suppressNoFilesError: true              // injects content + metadata into a template
+  .use(layouts({               // injects content + metadata into a template
+    suppressNoFilesError: true              
   }))
   .use(debug())
   .use(assets({
